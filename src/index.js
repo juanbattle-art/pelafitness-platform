628adf9ea6f389a732855590d4fdc7fb51198349
juanbattle-art <juanbattle@hotmail.com>
2026-05-07 16:21:44 -0300

    V

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<React.StrictMode><App /></React.StrictMode>)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
+
+// Registrar Service Worker para PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => console.log('SW registrado:', registration.scope))
+      .catch(err => console.log('SW error:', err));
+  });
+}
