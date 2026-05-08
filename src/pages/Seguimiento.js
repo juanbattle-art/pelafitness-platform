f23af45877138f67c6b8095b81326acc847da1f7
juanbattle-art <juanbattle@hotmail.com>
2026-05-08 16:02:44 -0300

    Update Seguimiento.js

--- src/pages/Seguimiento.js
+++ src/pages/Seguimiento.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 
